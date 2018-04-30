988a7fe9c48b7b525dc8825b49125bffa7749280
Nadasan Raul <nadasanraul@gmail.com>
2018-04-30 11:13:10 +0200

    Formating the expense amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
-import moment from 'moment'
+import moment from 'moment';
+import numeral from 'numeral';
 import {removeExpense} from '../actions/expenses';
 
 export const ExpenseListItem = ({id, description, amount, createdAt, dispatch}) => (
     <div className="card border-primary mb-3">
         <div className="card-body">
             <h2 className="card-title">{description}</h2>
-            <p className="card-text">Amount:{amount} </p>
+            <p className="card-text">Amount:{numeral(amount / 100).format('$0,0.00')} </p>
             <p className="card-text">On: {moment(createdAt).format('Do MMM, YYYY')}</p>
             <button className="btn btn-danger" onClick={(e) => {
                 dispatch(removeExpense({id}));
             }}>Remove</button>
-            <Link to={`edit/${id}`} className="btn btn-info">Edit</Link>
+            <Link to={`edit/${id}`} className="btn btn-info float-right">Edit</Link>
         </div>
     </div>
 );
